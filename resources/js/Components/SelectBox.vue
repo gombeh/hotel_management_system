8459bc8e7f74411001bc87b41c2aad59e8e7804f
rasoul <r.zinati1372@gmail.com>
2025-10-02 01:59:58 +0330

    fix select box component fix errors

--- resources/js/Components/SelectBox.vue
+++ resources/js/Components/SelectBox.vue
@@ -1,20 +1,19 @@
 <template>
-    <div class="form-label">Select Roles</div>
+    <label v-if="label" class="form-label">{{ label }}</label>
     <select class="form-select"
-            :class="{ 'is-invalid':modelError.length}"
+            :class="{ 'is-invalid':error}"
             v-bind="$attrs"
             :id="name"
             v-model="localValue">
         <slot/>
     </select>
-    <div class="invalid-feedback" v-if="modelError.length">
-        {{ errors[modelError[0]] }}
+    <div class="invalid-feedback" v-if="error">
+        {{ error }}
     </div>
 </template>
 
 <script setup>
-import {computed, defineProps, ref, watch} from 'vue'
-import {wildCardProperty} from "../Utils/helper.js";
+import {defineProps, ref, watch} from 'vue'
 
 const props = defineProps({
     name: String,
@@ -26,16 +25,17 @@ const props = defineProps({
         type: [String, Number, Array, null],
         default: null
     },
-    errors: {
-        type: Object,
-        default: () => ({})
-    },
+    error: String
 })
 const localValue = ref(props.modelValue);
 const emit = defineEmits(['update:modelValue'])
 
-const modelError = computed(() => wildCardProperty(props.errors, new RegExp(`^${props.name}.\\d+$`)));
-
 watch(localValue, (value) => emit('update:modelValue', value))
 
 </script>
+
+<style>
+.form-select.is-invalid:not([multiple]):not([size]) {
+    --tblr-form-select-bg-icon: null,
+}
+</style>
