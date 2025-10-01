3b08215e7036738f11ed139ddb81d2a7b21bcf22
rasoul <r.zinati1372@gmail.com>
2025-10-01 21:00:40 +0330

    multi select component

---
+++ resources/js/Components/MultiSelect.vue
@@ -0,0 +1,61 @@
+<template>
+    <label v-if="label" class="form-label">{{label}}</label>
+    <multiselect
+        :options="convertOptions"
+        v-model="selected"
+        :taggable="true"
+        label="label"
+        track-by="value"
+        ref="multiselectRef"
+        :placeholder="placeholder"
+        :class="{'invalid' : error}"
+        :multiple="true">
+    </multiselect>
+    <div class="invalid-feedback d-block" v-if="error">
+        {{ error }}
+    </div>
+</template>
+
+<script setup>
+import Multiselect from 'vue-multiselect'
+import "vue-multiselect/dist/vue-multiselect.min.css";
+import {ref, watch} from "vue";
+
+const props = defineProps({
+    label: String,
+    modelValue: Array,
+    options: Object,
+    placeholder: {
+        type: String,
+        default: "Choose your items"
+    },
+    error: String
+})
+
+const emits = defineEmits(["update:modelValue"])
+
+const convertOptions = Object.entries(props.options).map(([key, value]) => ({
+    label: value,
+    value: key
+}))
+
+const selected = ref(props.modelValue.map(value => ({
+    label: convertOptions.value,
+    value: value
+})));
+
+watch(selected, (value) => {
+    emits('update:modelValue', value.map(val => val.value))
+})
+</script>
+
+<style>
+.multiselect {
+    border-radius: var(--tblr-border-radius);
+}
+
+.invalid {
+    border: 1px solid;
+    border-color: var(--tblr-form-invalid-border-color);
+}
+</style>
